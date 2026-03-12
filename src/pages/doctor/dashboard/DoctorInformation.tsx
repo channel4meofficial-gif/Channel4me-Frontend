0ac5485e47c924d1e85a7853634c188c374ba54a
Muhammadh <muhammadh.20231833@iit.ac.lk>
2026-03-13 02:19:54 +0530

    code clean 3

--- src/pages/doctor/dashboard/DoctorInformation.tsx
+++ src/pages/doctor/dashboard/DoctorInformation.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import '../../../styles/doctor/dashboard/DcotorInformation.css';
 
-
 function DoctorInformation() {
   const navigate = useNavigate();
 
