34617900696494cab98fdf3c84a09d8cbaa9c577
manuji2003 <manuji.20241442@iit.ac.lk>
2026-03-14 16:07:08 +0530

    feat: update App.tsx with DoctorPage route

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-// import other pages if they exist
+import DoctorPage from './pages/doctor-booking/DoctorPage';
+// import DoctorProfilePage from './pages/doctor-booking/DoctorProfilePage';
+// import DoctorSessionsPage from './pages/doctor-booking/DoctorSessionsPage';
+// import DoctorBookingPage from './pages/doctor-booking/DoctorBookingPage';
+// import CardPaymentPage from './pages/doctor-booking/CardPaymentPage';
+// import PaymentPage from './pages/doctor-booking/PaymentPage';
+// import PaymentReceiptPage from './pages/doctor-booking/PaymentReceiptPage';
 
 function ScrollToHash() {
     const location = useLocation();
-
     useEffect(() => {
         if (location.hash) {
-            const id = location.hash.replace('#', '');
-            const element = document.getElementById(id);
-            if (element) {
-                element.scrollIntoView({ behavior: 'smooth' });
-            }
-        } else {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
+            const el = document.querySelector(location.hash);
+            if (el) el.scrollIntoView({ behavior: 'smooth' });
         }
     }, [location]);
-
     return null;
 }
 
@@ -28,7 +27,13 @@ function App() {
             <ScrollToHash />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                {/* Add other routes like /about, /contact if they exist */}
+                <Route path="/doctors" element={<DoctorPage />} />
+                {/* <Route path="/doctor/:id" element={<DoctorProfilePage />} /> */}
+                {/* <Route path="/doctor/:id/sessions" element={<DoctorSessionsPage />} /> */}
+                {/* <Route path="/doctor/:id/booking" element={<DoctorBookingPage />} /> */}
+                {/* <Route path="/card-payment" element={<CardPaymentPage />} /> */}
+                {/* <Route path="/payment" element={<PaymentPage />} /> */}
+                {/* <Route path="/payment-receipt" element={<PaymentReceiptPage />} /> */}
             </Routes>
         </BrowserRouter>
     );
